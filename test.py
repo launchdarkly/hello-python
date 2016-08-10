b889f8e5e2b10da26f7f10fa370c6fdf8e1f860e
Dan Richelson <dan@launchdarkly.com>
2016-08-10 16:37:46 -0700

    Add logging

--- test.py
+++ test.py
@@ -1,5 +1,16 @@
 #!/usr/bin/env python
+import logging
+import sys
+
 import ldclient
+
+root = logging.getLogger()
+root.setLevel(logging.INFO)
+ch = logging.StreamHandler(sys.stdout)
+ch.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+root.addHandler(ch)
 
 if __name__ == "__main__":
   ldclient.sdk_key= "YOUR_SDK_KEY"
