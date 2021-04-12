02f58fbaa4788d8ae96e6bf7df39675c5170023f
Eli Bishop <eli@launchdarkly.com>
2021-04-12 13:43:40 -0700

    update to use current demo standards (#10)

--- README.md
+++ README.md
@@ -1,6 +1,21 @@
-### LaunchDarkly Sample Python Application  ###
-We've built a simple console application that demonstrates how LaunchDarkly's SDK works.  Below, you'll find the basic build procedure, but for more comprehensive instructions, you can visit your [Quickstart page](https://app.launchdarkly.com/quickstart#/).
-##### Build instructions  #####
-1. Install the LaunchDarkly Python SDK by running `pip install -r requirements.txt`
-2. Copy your SDK key and feature flag key from your LaunchDarkly dashboard into `test.py` 
-3. Run `python test.py`
+# LaunchDarkly Sample Python Application 
+
+ Below, you'll find the basic build procedure, but for more comprehensive instructions, you can visit your [Quickstart page](https://app.launchdarkly.com/quickstart#/) or the [Python SDK reference guide](https://docs.launchdarkly.com/sdk/server-side/python).
+
+This demo requires Python version 3.5 or higher.
+
+## Build instructions 
+
+1. Install the LaunchDarkly Python SDK by running `pip install requirements.txt`
+
+2. Edit `test.py` and set the value of `sdk_key` to your LaunchDarkly SDK key. If there is an existing boolean feature flag in your LaunchDarkly project that you want to evaluate, set `feature_flag_key` to the flag key.
+
+```python
+sdk_key = "1234567890abcdef"
+
+feature_flag_key = "my-flag"
+```
+
+3. Run `python test.py`.
+
+You should see the message `"Feature flag '<flag key>' is <true/false> for this user"`.
--- test.py
+++ test.py
@@ -1,37 +1,45 @@
 #!/usr/bin/env python
-import logging
-import sys
-
 import ldclient
 from ldclient.config import Config
 
-root = logging.getLogger()
-root.setLevel(logging.INFO)
-ch = logging.StreamHandler(sys.stdout)
-ch.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-ch.setFormatter(formatter)
-root.addHandler(ch)
+# Set sdk_key to your LaunchDarkly SDK key before running
+sdk_key = ""
 
+# Set feature_flag_key to the feature flag key you want to evaluate
+feature_flag_key = "my-boolean-flag"
+
+def show_message(s):
+  print("*** %s" % s)
+  print()
 
 if __name__ == "__main__":
-  sdk_key = "YOUR_SDK_KEY"
+  if not sdk_key:
+    show_message("Please edit test.py to set sdk_key to your LaunchDarkly SDK key first")
+    exit()
+
   ldclient.set_config(Config(sdk_key))
 
+  # The SDK starts up the first time ldclient.get() is called
+  if ldclient.get().is_initialized():
+    show_message("SDK successfully initialized!")
+  else:
+    show_message("SDK failed to initialize")
+    exit()
+
+  # Set up the user properties. This user should appear on your LaunchDarkly users dashboard
+  # soon after you run the demo.
   user = {
-    "key": "bob@example.com",
-    "firstName": "Bob",
-    "lastName": "Loblaw",
-    "custom": {
-      "groups": "beta_testers"
-    }
+    "key": "example-user-key",
+    "name": "Sandy"
   }
 
-  show_feature = ldclient.get().variation("YOUR_FLAG_KEY", user, False)
+  flag_value = ldclient.get().variation(feature_flag_key, user, False)
 
-  if show_feature:
-    print("Showing your feature")
-  else:
-    print("Not showing your feature")
+  show_message("Feature flag '%s' is %s for this user" % (feature_flag_key, flag_value))
 
-  ldclient.get().close() # close the client before exiting the program - ensures that all events are delivered
+  # Here we ensure that the SDK shuts down cleanly and has a chance to deliver analytics
+  # events to LaunchDarkly before the program exits. If analytics events are not delivered,
+  # the user properties and flag usage statistics will not appear on your dashboard. In a
+  # normal long-running application, the SDK would continue running and events would be
+  # delivered automatically in the background.
+  ldclient.get().close()
